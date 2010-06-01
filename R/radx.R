d92ada9f8ad11c2d47e494b20bd710dcd3685e17
chillu <quantumelixir@gmail.com>
2010-06-01 19:13:46 +0530

    Allow creation of Multivariate UTP
    Adds the "ndirs" attribute to each radx object that specifies the number
    of directions. Example: For a function f:R^M -> R it is M.

--- R/radx.R
+++ R/radx.R
@@ -5,40 +5,49 @@
 #   a. Computer answer to each case explicity instead of delegating
 #   b. Write code in C
 
-radx <- function (val, type="variable", ord=1) {
-    if (ord < 0 )
-        stop("Error: radx objects must have ord >= 0")
+radx <- function (val, type="variable", ord=1, ndirs=1, wdir=1) {
+    if (ord < 0 || ndirs < 1)
+        stop("Error: radx objects must have ord >= 0, dirs >= 1")
+
+    if (wdir > ndirs)
+        stop("Error: wdir greater than ndirs")
 
     ad <- list()
     if (type == "variable") {
         if (ord == 0)
             ad$coeff <- c(val)
         else
-            ad$coeff <- c(val, 1, rep(0, ord - 1))
+            ad$coeff <- c(val, rep(rep(0, ord), ndirs))
+            ad$coeff[2 + ord * (wdir - 1)] <- 1
     }
-    else #constant
-        ad$coeff <- c(val, rep(0, ord))
+    else if (type == "constant")
+        ad$coeff <- c(val, rep(rep(0, ord), ndirs))
+    else
+        stop('Bad type: Choose between "variable" and "constant"')
 
     ad$ord <- ord
+    ad$ndirs <- ndirs
+
     class(ad) <- "radx"
 
     return(ad)
 }
 
-radx_from <- function (coeffs) {
+radx_from <- function (coeffs, ndirs=1) {
     ad <- list()
+    ad$ndirs <- ndirs
     ad$coeff <- c(coeffs)
-    ad$ord <- length(ad$coeff) - 1
+    ad$ord <- (length(ad$coeff) - 1)/ndirs
     class(ad) <- "radx"
 
     return(ad)
 }
 
-radx_empty <- function (ord) {
-    if (ord < 0 )
-        stop("Error: radx objects must have ord >= 0")
+radx_empty <- function (ord, ndirs=1) {
+    if (ord < 0 || ndirs < 1)
+        stop("Error: radx objects must have ord >= 0, dirs >= 1")
 
-    return(radx_from(rep(0, ord + 1)))
+    return(radx_from(c(0, rep(rep(0, ord), ndirs)), ndirs))
 }
 
 print.radx <- function (x) {
@@ -52,6 +61,7 @@ print.radx <- function (x) {
         cat("Derivatives: NA\n")
 
     cat("Order      : ", x$ord, "\n", sep="")
+    cat("Directions : ", x$ndirs, "\n", sep="")
     invisible(x)
 }
 
