a11cb98b255dce92b3c8ac11036d066d9115cb1e
chillu <quantumelixir@gmail.com>
2010-06-02 12:24:22 +0530

    Extend elementary functions to multivariate UTP
    exp, log, ^ (pow), sin, cos now work on multivariate UTP objects i.e
    radx objects with ndirs > 1

--- R/elementary.R
+++ R/elementary.R
@@ -3,61 +3,75 @@
 
 "exp.radx" <- function(x) {
     if(class(x) == "radx") {
-        coeff <- rep(0, x$ord + 1)
+        coeff <- rep(0, length(x$coeff))
         coeff[1] <- exp(x$coeff[1])
-        for (k in seq(x$ord)) {
-            i <- 1
-            while (i <= k) {
-                coeff[k + 1] <- coeff[k + 1] + i * x$coeff[i + 1] * coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                i <- i + 1
+        for (p in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    coeff[s + d + 1] <- coeff[s + d + 1] + i * x$coeff[s + i + 1] * coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    i <- i + 1
+                }
+                coeff[s + d + 1] <- coeff[s + d + 1] + d * x$coeff[s + d + 1] * coeff[1] / base::factorial(d)
+
+                coeff[s + d + 1] <- coeff[s + d + 1] * base::factorial(d - 1)
             }
-            coeff[k + 1] <- coeff[k + 1] * base::factorial(k - 1)
-        }
     }
     else { #numeric
         return(exp(x))
     }
 
-    return(radx_from(coeff))
+    return(radx_from(coeff, ndirs=x$ndirs))
 }
 
 # natural logarithm (base e)
 "log.radx" <- function(x) {
     if(class(x) == "radx") {
-        coeff <- rep(0, x$ord + 1)
+        coeff <- rep(0, length(x$coeff))
         coeff[1] <- log(x$coeff[1])
-        for (k in seq(x$ord)) {
-            i <- 1
-            while (i < k) {
-                coeff[k + 1] <- coeff[k + 1] + i * coeff[i + 1] * x$coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                i <- i + 1
+        for (p in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    coeff[s + d + 1] <- coeff[s + d + 1] + i * coeff[s + i + 1] * x$coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    i <- i + 1
+                }
+
+                coeff[s + d + 1] <- (x$coeff[s + d + 1] - base::factorial(d - 1) * coeff[s + d + 1]) / x$coeff[1]
             }
-            coeff[k + 1] <- (x$coeff[k + 1] - base::factorial(k - 1) * coeff[k + 1]) / x$coeff[1]
-        }
     }
     else { #numeric
         return(log(x))
     }
 
-    return(radx_from(coeff))
+    return(radx_from(coeff, ndirs=x$ndirs))
 }
 
-# a ^ x and x ^ a (TODO: x ^ x)
+# a ^ x and x ^ a
 "^.radx" <- function(x, y) {
     if(class(x) == "radx" && class(y) == "radx") { # x ^ x
         return(exp(y * log(x)))
     }
     else if(class(x) == "radx") {
-        coeff <- rep(0, x$ord + 1)
+        coeff <- rep(0, length(x$coeff))
         coeff[1] <- x$coeff[1] ^ y
-        for (k in seq(x$ord)) {
-            i <- 1
-            while (i <= k) {
-                coeff[k + 1] <- coeff[k + 1] + ((y + 1) ^ i / k - 1) * x$coeff[i + 1] * coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                i <- i + 1
+        for (p in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    coeff[s + d + 1] <- coeff[s + d + 1] + ((y + 1) ^ i / d - 1) * x$coeff[s + i + 1] * coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    i <- i + 1
+                }
+                coeff[s + d + 1] <- coeff[s + d + 1] + ((y + 1) ^ d / d - 1) * x$coeff[s + d + 1] * coeff[1] / base::factorial(d)
+
+                coeff[s + d + 1] <- base::factorial(d) * coeff[s + d + 1] / x$coeff[1]
             }
-            coeff[k + 1] <- base::factorial(k) * coeff[k + 1] / x$coeff[1]
-        }
     }
     else if(class(y) == "radx") {
         return(exp(y * log(x)))
@@ -66,55 +80,67 @@
         return(x ^ y)
     }
 
-    return(radx_from(coeff))
+    return(radx_from(coeff, ndirs=x$ndirs))
 }
 
 "sin.radx" <- function(x) {
     if(class(x) == "radx") {
-        s_coeff <- rep(0, x$ord + 1)
-        c_coeff <- rep(0, x$ord + 1)
+        s_coeff <- rep(0, length(x$coeff))
+        c_coeff <- rep(0, length(x$coeff))
 
         s_coeff[1] <- sin(x$coeff[1])
         c_coeff[1] <- cos(x$coeff[1])
-        for (k in seq(x$ord)) {
-            i <- 1
-            while (i <= k) {
-                s_coeff[k + 1] <- s_coeff[k + 1] + i * x$coeff[i + 1] * c_coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                c_coeff[k + 1] <- c_coeff[k + 1] + i * x$coeff[i + 1] * s_coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                i <- i + 1
+        for (k in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    s_coeff[s + d + 1] <- s_coeff[s + d + 1] + i * x$coeff[s + i + 1] * c_coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    c_coeff[s + d + 1] <- c_coeff[s + d + 1] + i * x$coeff[s + i + 1] * s_coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    i <- i + 1
+                }
+                s_coeff[s + d + 1] <- s_coeff[s + d + 1] + d * x$coeff[s + d + 1] * c_coeff[1] / base::factorial(d)
+                c_coeff[s + d + 1] <- c_coeff[s + d + 1] + d * x$coeff[s + d + 1] * s_coeff[1] / base::factorial(d)
+
+                s_coeff[s + d + 1] <- base::factorial(d - 1) * s_coeff[s + d + 1]
+                c_coeff[s + d + 1] <- base::factorial(d - 1) * c_coeff[s + d + 1] * -1
             }
-            s_coeff[k + 1] <- base::factorial(k - 1) * s_coeff[k + 1]
-            c_coeff[k + 1] <- base::factorial(k - 1) * c_coeff[k + 1] * -1
-        }
     }
     else { #numeric
         return(sin(x))
     }
 
-    return(radx_from(s_coeff))
+    return(radx_from(s_coeff, ndirs=x$ndirs))
 }
 
 "cos.radx" <- function(x) {
     if(class(x) == "radx") {
-        s_coeff <- rep(0, x$ord + 1)
-        c_coeff <- rep(0, x$ord + 1)
+        s_coeff <- rep(0, length(x$coeff))
+        c_coeff <- rep(0, length(x$coeff))
 
         s_coeff[1] <- sin(x$coeff[1])
         c_coeff[1] <- cos(x$coeff[1])
-        for (k in seq(x$ord)) {
-            i <- 1
-            while (i <= k) {
-                s_coeff[k + 1] <- s_coeff[k + 1] + i * x$coeff[i + 1] * c_coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                c_coeff[k + 1] <- c_coeff[k + 1] + i * x$coeff[i + 1] * s_coeff[k - i + 1] / (base::factorial(i) * base::factorial(k - i))
-                i <- i + 1
+        for (p in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    s_coeff[s + d + 1] <- s_coeff[s + d + 1] + i * x$coeff[s + i + 1] * c_coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    c_coeff[s + d + 1] <- c_coeff[s + d + 1] + i * x$coeff[s + i + 1] * s_coeff[s + d - i + 1] / (base::factorial(i) * base::factorial(d - i))
+                    i <- i + 1
+                }
+                s_coeff[s + d + 1] <- s_coeff[s + d + 1] + d * x$coeff[s + i + 1] * c_coeff[1] / base::factorial(d)
+                c_coeff[s + d + 1] <- c_coeff[s + d + 1] + d * x$coeff[s + i + 1] * s_coeff[1] / base::factorial(d)
+
+                s_coeff[s + d + 1] <- base::factorial(d - 1) * s_coeff[s + d + 1]
+                c_coeff[s + d + 1] <- base::factorial(d - 1) * c_coeff[s + d + 1] * -1
             }
-            s_coeff[k + 1] <- base::factorial(k - 1) * s_coeff[k + 1]
-            c_coeff[k + 1] <- base::factorial(k - 1) * c_coeff[k + 1] * -1
-        }
     }
     else { #numeric
         return(cos(x))
     }
 
-    return(radx_from(c_coeff))
+    return(radx_from(c_coeff, ndirs=x$ndirs))
 }
