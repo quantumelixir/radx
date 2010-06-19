a9a5fcb0ea424c24e996e79e57b033ed22d4b19d
chillu <quantumelixir@gmail.com>
2010-06-19 20:38:30 +0530

    Fix kludgy genMultiIndices
    By nesting the recursive function, fillMultiIndices, within
    genMultiIndices we can do away with a global matrix to store the
    multi-indices.

--- R/mindex.R
+++ R/mindex.R
@@ -1,31 +1,29 @@
-# the global multi-index matrix with one mult-index per row
-mat <- matrix()
-
-# fillMultiIndices: Helper function for genMultiIndices
-# Fill choose(n + v - 1, v) multi-indices of length n
-# and sum v in the matrix mat starting from (i,j)
-fillMultiIndices <- function(i, j, n, v) {
-    if (n == 1) {
-        mat[i, j] <<- v
-    }
-    else if (v == 0) {
-        mat[i, j:(j + n - 1)] <<- 0
-    }
-    else {
-        rowOffset <- 0
-        # the first element of each multi-index can be any of 0, 1, ..., v
-        for (k in v:0) {
-            times <- choose((n - 1) + (v - k) - 1, (v - k))
-            mat[(i + rowOffset):(i + rowOffset + times - 1), j] <<- k
-            fillMultiIndices(i + rowOffset, j + 1, n - 1, v - k)
-            rowOffset <- rowOffset + times
-        }
-    }
-}
-
 # Generate multi-indices of given length (N) and absolute value (V)
 genMultiIndices <- function(N, V) {
-    mat <<- matrix(nrow=choose(N + V - 1, V), ncol=N)
+    mat <- matrix(nrow=choose(N + V - 1, V), ncol=N)
+
+    # fillMultiIndices: Helper function for genMultiIndices
+    # Fill choose(n + v - 1, v) multi-indices of length n
+    # and sum v in the matrix mat starting from (i,j)
+    fillMultiIndices <- function(i, j, n, v) {
+        if (n == 1) {
+            mat[i, j] <<- v
+        }
+        else if (v == 0) {
+            mat[i, j:(j + n - 1)] <<- 0
+        }
+        else {
+            rowOffset <- 0
+            # the first element of each multi-index can be any of 0, 1, ..., v
+            for (k in v:0) {
+                times <- choose((n - 1) + (v - k) - 1, (v - k))
+                mat[(i + rowOffset):(i + rowOffset + times - 1), j] <<- k
+                fillMultiIndices(i + rowOffset, j + 1, n - 1, v - k)
+                rowOffset <- rowOffset + times
+            }
+        }
+    }
+
     fillMultiIndices(1, 1, N, V)
     return(mat)
 }
@@ -61,10 +59,10 @@ gammaMultiIndex <- function(i, j) {
 
     gij <- 0
     for (a in 1:d) {
-        genMultiIndices(n, a)
-        total <- dim(mat)[1]
+        J <- genMultiIndices(n, a)
+        total <- dim(J)[1]
         for (b in 1:total) {
-            k <- mat[b,]
+            k <- J[b,]
             gij <- gij + singleTerm(i, j, k, d)
         }
     }
@@ -78,10 +76,10 @@ gammaRow <- function(i) {
     V <- absolute(i)
     P <- choose(N + V - 1, V)
 
-    genMultiIndices(N, V)
+    J <- genMultiIndices(N, V)
     gammarow <- matrix(nrow=1, ncol=P)
     for (j in 1:P) {
-        gammarow[j] <- gammaMultiIndex(i, mat[j,])
+        gammarow[j] <- gammaMultiIndex(i, J[j,])
     }
     return(gammarow)
 }
@@ -89,42 +87,13 @@ gammaRow <- function(i) {
 # gamma matrix stores interpolation coefficients for all
 # (pure and mixed) derivatives of order V in N variables
 gammaMatrix <- function(N, V) {
-    genMultiIndices(N, V)
+    J <- genMultiIndices(N, V)
     P <- choose(N + V - 1, V)
     gammamatrix <- matrix(0, nrow=P, ncol=P)
     for (i in 1:P) {
         for (j in 1:P) {
-            gammamatrix[i,j] <- gammaMultiIndex(mat[i,], mat[j,])
+            gammamatrix[i,j] <- gammaMultiIndex(J[i,], J[j,])
         }
     }
     return(gammamatrix)
-}
-
-# perform testing
-gtest <- function(N, V) {
-    genMultiIndices(N, V)
-    mindices <- dim(mat)[1]
-    gammamatrix <- matrix(0, nrow=mindices, ncol=mindices)
-
-    nonzero <- 0
-    for (i in 1:mindices) {
-    for (j in 1:mindices) {
-        gammamatrix[i,j] <- gammaMultiIndex(mat[i,], mat[j,])
-        if (abs(gammamatrix[i, j]) > 1e-6)
-            nonzero <- nonzero + 1
-    }
-    }
-
-    mval <- 0
-    for (i in 1:mindices) {
-        cur <- sum(abs(gammamatrix[i,]))
-        if (mval < cur)
-            mval = cur
-    }
-
-    cat('N =                ', N, '\n')
-    cat('D =                ', V, '\n')
-    cat('Nonzero Entries   :', nonzero, '\n')
-    cat('Max_i (Sum_j g_ij):', mval, '\n')
-    print(gammamatrix)
 }
