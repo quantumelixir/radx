d0bd86db5718cfe792b139d8dd25d664ff098c6e
chillu <quantumelixir@gmail.com>
2010-06-20 12:33:23 +0530

    Add binary operator atan for inverse tangent

--- R/elementary.R
+++ R/elementary.R
@@ -143,3 +143,33 @@
 
     return(radx_from(c_coeff, ndirs=x$ndirs))
 }
+
+"atan.radx" <- function(x) {
+    if(class(x) == "radx") {
+        p_coeff <- rep(0, length(x$coeff))
+        w_coeff <- rep(0, length(x$coeff))
+
+        p_coeff[1] <- atan(x$coeff[1])
+        w_coeff[1] <- (1 + x$coeff[1]^2)
+        for (p in seq(x$ndirs))
+            for (d in seq(x$ord)) {
+                s <- (p - 1) * x$ord
+
+                i <- 1
+                while (i < d) {
+                    p_coeff[s + d + 1] <- p_coeff[s + d + 1] + i * p_coeff[s + i + 1] * w_coeff[s + d - i + 1]
+                    w_coeff[s + d + 1] <- w_coeff[s + d + 1] + i * x$coeff[s + i + 1] * x$coeff[s + d - i + 1]
+                    i <- i + 1
+                }
+                w_coeff[s + d + 1] <- w_coeff[s + d + 1] + d * x$coeff[s + d + 1] * x$coeff[1]
+
+                p_coeff[s + d + 1] <- (d * x$coeff[s + d + 1] - p_coeff[s + d + 1])/(d * w_coeff[1])
+                w_coeff[s + d + 1] <- 2 * w_coeff[s + d + 1] / d
+            }
+    }
+    else { #numeric
+        return(atan(x))
+    }
+
+    return(radx_from(p_coeff, ndirs=x$ndirs))
+}
